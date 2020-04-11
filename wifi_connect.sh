7f616bd15b97623881d35a8f911bb4de6bb058f2
Jayme Anchante <jayme.anchante@disroot.org>
2020-04-11 15:31:44 -0300

    :sparkles: making sure all wpa_supplicant processes are killed

--- wifi_connect.sh
+++ wifi_connect.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+
+processes=$(ps aux | grep wpa_supplicant | wc -l)
+threshold=1
+while [ $processes -gt $threshold ]
+do
+	killall wpa_supplicant
+	echo killing wpa_supplicant
+	processes=$(ps aux | grep wpa_supplicant | wc -l)
+done
 
 rc-service networkmanager stop
 ip link set wlan0 up
