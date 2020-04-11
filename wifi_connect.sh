6b884a042c6673b54ade04f33b08c25b37ec4109
Jayme Anchante <jayme.anchante@disroot.org>
2020-04-11 15:13:10 -0300

    :pencil2: fixed typo in wifi_coonect.sh

--- wifi_connect.sh
+++ wifi_connect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rc-service networkmanager stop
-ip link setp wlan0 up
+ip link set wlan0 up
 killall wpa_supplicant
 wpa_supplicant -B -Dwext -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
 dhclient -v wlan0
